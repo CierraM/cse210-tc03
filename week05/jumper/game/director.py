68d652887d128528dd4a0eca4c09cee13453b5cf
Cierra <4cibear@gmail.com>
2021-02-03 16:18:36 -0700

    wrote director skeleton

---
+++ week05/jumper/game/director.py
@@ -0,0 +1,24 @@
+from game.console import console
+from game.jumper import jumper
+from game.word import word
+
+class Director:
+    """
+        
+    """
+    def __init__(self):
+
+    def start_game():
+
+    def get_inputs():
+        
+
+    def do_updates():
+
+    def do_outputs():
+
+    
+
+    
+
+    
