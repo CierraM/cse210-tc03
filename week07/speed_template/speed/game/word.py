472c45a4f9fc53093851884921ae6ea881c1d568
BenjaminFern <66424102+BenjaminFern@users.noreply.github.com>
2021-02-17 17:30:05 -0700

    edited the word class and rearranged part of the directory for the word class to work.

--- week07/speed_template/speed/game/word.py
+++ week07/speed_template/speed/game/word.py
@@ -3,9 +3,7 @@ from game import constants
 from game.actor import Actor
 from game.point import Point
 
-# CHANGE THIS ONE
-
-class Food(Actor):
+class Word(Actor):
     """Delicious red apples for an herbivorous snake to eat
         In charge of keeping track of appearance and position of food.
 
@@ -17,21 +15,24 @@ class Food(Actor):
     """
     def __init__(self):
         super().__init__()
-        self._points = 0
-        self.set_text('@')
-        self.set_position(Point(0, 0))
-        self.set_velocity((0, 0))
-        self.reset()
+        # self._points = 0
+        # self.set_text('@')
+        # self.set_position(Point(0, 0))
+        self.set_velocity((0.5, 0))
+        # self.reset()
 
-    def get_points(self):
-        """
-        Returns the current number of points
-        """
-        self._points = random.randint(1, 5)
-        return self._points
+        self.word_library = constants.LIBRARY
 
-    def reset(self):
+    def send_word(self):
         """
-        Resets the position of the food
+        This class will return word_library when called
+
+            self.word_library: a list of all the words from constant "library"
         """
-        self.set_position(Point((random.randint(1, 60)), (random.randint(1,20))))
+        return self.word_library[random.randint(0, len(self.word_library) - 1)]
+
+    # def reset(self):
+    #     """
+    #     Resets the position of the food
+    #     """
+    #     self.set_position(Point((random.randint(1, 60)), (random.randint(1,20))))
