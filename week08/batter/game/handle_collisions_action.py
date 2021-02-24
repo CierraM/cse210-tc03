db5d1cae80d7eab85bd2e75d906606e0d2d56ab4
Cierra <4cibear@gmail.com>
2021-02-24 11:26:00 -0700

    made paddle able to move

--- week08/batter/game/handle_collisions_action.py
+++ week08/batter/game/handle_collisions_action.py
@@ -4,6 +4,9 @@ from game.action import Action
 
 class HandleCollisionsAction(Action):
     """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
+        check if ball collides with paddle or brick, or wall
+        Do something to change ball direction
+        Do something to make the brick it hit disappear
     
     Stereotype:
         Controller
@@ -15,11 +18,11 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        marquee = cast["marquee"][0] # there's only one
-        robot = cast["robot"][0] # there's only one
-        artifacts = cast["artifact"]
-        marquee.set_text("")
-        for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()):
-                description = artifact.get_description()
-                marquee.set_text(description) 
+        # marquee = cast["marquee"][0] # there's only one
+        # robot = cast["robot"][0] # there's only one
+        # artifacts = cast["artifact"]
+        # marquee.set_text("")
+        # for artifact in artifacts:
+        #     if robot.get_position().equals(artifact.get_position()):
+        #         description = artifact.get_description()
+        #         marquee.set_text(description) 
--- week08/batter/game/input_service.py
+++ week08/batter/game/input_service.py
@@ -17,10 +17,9 @@ class InputService:
         """The class constructor."""
         self._screen = screen
         self._keys = {}
-        self._keys[119] = Point(0, -1) # w
-        self._keys[115] = Point(0, 1) # s
         self._keys[97] = Point(-1, 0) # a
         self._keys[100] = Point(1, 0) # d
+
         
     def get_direction(self):
         """Gets the selected direction for the given player.
@@ -31,7 +30,9 @@ class InputService:
         direction = Point(0, 0)
         event = self._screen.get_event()
         if isinstance(event, KeyboardEvent):
-            if event.key_code == 27:
+            if event.key_code == -1 or event.key_code == 27:
                 sys.exit()
-            direction = self._keys.get(event.key_code, Point(0, 0))
+            elif event.key_code == 97 or event.key_code == 100:
+                direction = self._keys[event.key_code]
+            
         return direction
