7e385975d1688bebe2c48cf4eac5398483fc95b2
Cierra <4cibear@gmail.com>
2021-02-24 10:45:22 -0700

    added classes from rfk

---
+++ week08/batter/game/draw_actors_action.py
@@ -0,0 +1,25 @@
+from game.action import Action
+
+# TODO: Define the DrawActorsAction class here
+class DrawActorsAction(Action):
+    """An action to draw actors on the screen when called
+    
+    Stereotype:
+        controller
+
+    Attributes:
+        output: an instance of output_service
+    """
+    def __init__(self, output):
+        self._output = output
+
+    def execute(self, cast):
+        """Executes action on given actors by drawing them to the screen
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+        self._output.clear_screen()
+        for value in cast.values():
+            
+            self._output.draw_actors(value)
+        self._output.flush_buffer()
+
