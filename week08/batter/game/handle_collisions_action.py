745b34d02735167b1579ec9e25df112a8eb6b7f2
BenjaminFern <66424102+BenjaminFern@users.noreply.github.com>
2021-02-24 17:42:37 -0700

    Fixed the collision with bricks so that they collide with the ball and disappear. Comments are still needed and also ball offset (in order to hit all of the blocks)

--- week08/batter/game/handle_collisions_action.py
+++ week08/batter/game/handle_collisions_action.py
@@ -12,9 +12,12 @@ class HandleCollisionsAction(Action):
     Stereotype:
         Controller
     """
-    def wall_bounce(self, ball, jester):
+    def __init__(self):
+        self.count = 0
+
+    def wall_bounce(self, ball):
         point1 = ball.get_position()
-        jester.set_text(f"y = {str(point1.get_y())}")
+        # jester.set_text(f"y = {str(point1.get_y())}")
         if point1.get_y() == 1:
             ball.set_velocity(ball.get_velocity().reverse_y())
         if point1.get_x() == 1 or point1.get_x() == constants.MAX_X - 1:
@@ -22,15 +25,34 @@ class HandleCollisionsAction(Action):
         if point1.get_y() == constants.MAX_Y - 1:
             sys.exit(0)
 
-    def paddle_bounce(self, paddle, ball, jester):
+    def paddle_bounce(self, paddle, ball):
         ball_point = ball.get_position()
         paddle_left = paddle.get_position()
         paddle_right = len(paddle.get_text())
         if ball_point.get_y() == paddle_left.get_y() - 1:
             x = ball_point.get_x()
+            y = ball_point.get_y()
             paddle_x = paddle_left.get_x()
             if x >= paddle_x and x <= paddle_x + paddle_right:
                 ball.set_velocity(ball.get_velocity().reverse_y())
+
+    def brick_bounce(self, ball, bricks):
+        for i in range(len(bricks)):
+            if ball.get_position().equals(bricks[i].get_position()):
+                self.count += 1
+                bricks.pop(i)
+                ball.set_velocity(ball.get_velocity().reverse_y())
+                break
+            # elif ball.get_position().equals(bricks[i].get_position()) and bricks[i] == " ":
+            #     ball.set_velocity(ball.get_velocity().reverse_y())
+
+        # point1 = ball.get_position()
+        # if point1.get_y() == 1:
+        #     ball.set_velocity(ball.get_velocity().reverse_y())
+        # if point1.get_x() == 1 or point1.get_x() == constants.MAX_X - 1:
+        #     ball.set_velocity(ball.get_velocity().reverse_x())  
+        # if point1.get_y() == constants.MAX_Y - 1:
+        #     sys.exit(0)
 
 
 
@@ -42,9 +64,11 @@ class HandleCollisionsAction(Action):
         """
         ball = cast["ball"][0] # there's only one
         paddle = cast["paddle"][0]
-        jester = cast["jester"][0]
-        self.paddle_bounce(paddle, ball, jester)
-        self.wall_bounce(ball, jester)
+        bricks = cast["brick"]
+        # jester = cast["jester"][0]
+        self.paddle_bounce(paddle, ball)
+        self.wall_bounce(ball)
+        self.brick_bounce(ball, bricks)
 
         # robot = cast["robot"][0] # there's only one
         # artifacts = cast["artifact"]
